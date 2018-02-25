8a864df4aa8a741dce1cd4a933ff3f5b8a18b04a
Belchior Oliveira <belchior@outlook.com>
2018-02-25 17:44:09 -0300

    added action types file

---
+++ src/actionTypes.js
@@ -0,0 +1,8 @@
+export const ADD_SCORE = 'ADD_SCORE';
+export const BEST_SCORE = 'BEST_SCORE';
+export const MOVE_DOWN = 'MOVE_DOWN';
+export const MOVE_LEFT = 'MOVE_LEFT';
+export const MOVE_RIGHT = 'MOVE_RIGHT';
+export const MOVE_UP = 'MOVE_UP';
+export const ROLLBACK = 'ROLLBACK';
+export const TOGGLE_HARDMODE = 'TOGGLE_HARDMODE';
