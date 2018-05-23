80996fb0cd2e0ecaa0bde63cb6023f74caf0c324
Belchior Oliveira <belchior@outlook.com>
2018-05-22 22:03:11 -0300

    added shortcut handlers at App component

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,18 +1,29 @@
 import React, { Component } from 'react';
-import './App.css';
 
+import { moveToBottom, moveToLeft, moveToRight, moveToTop } from '../../actions/move';
+import { Keyboard } from '../keyboard/Keyboard';
 import Sidebar from '../Sidebar';
 import Wall from '../Wall';
+import './App.css';
+
 
 export class App extends Component {
   render() {
+
     return (
-      <div className="App">
-        <Sidebar></Sidebar>
-        <main className="main">
-          <Wall></Wall>
-        </main>
-      </div>
+      <Keyboard shortcuts={[
+        {shortcut: 'arrow-down', action: moveToBottom},
+        {shortcut: 'arrow-left', action: moveToLeft},
+        {shortcut: 'arrow-right', action: moveToRight},
+        {shortcut: 'arrow-up', action: moveToTop},
+      ]}>
+        <div className="App">
+          <Sidebar></Sidebar>
+          <main className="main">
+            <Wall></Wall>
+          </main>
+        </div>
+      </Keyboard>
     );
   }
 }
