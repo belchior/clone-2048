bc61176608915ae692ec7874b104d45100f50596
Belchior Oliveira <belchior@outlook.com>
2018-07-10 22:08:39 -0300

    App Component was updated with some unit tests and

--- src/components/App/App.connected.test.js
+++ src/components/App/App.connected.test.js
@@ -5,30 +5,30 @@ it('mapStateToProps should be a function', () => {
   expect(typeof mapStateToProps === 'function').toBe(true);
 });
 
-it('mapStateToProps should return un Object', () => {
+it('mapStateToProps should return an Object', () => {
   const props = mapStateToProps(initialState);
   expect(typeof props === 'object').toBe(true);
 });
 
-it('mapStateToProps should return un Object that contains the maxBlock key', () => {
+it('mapStateToProps should return an Object that contains the maxBlock key', () => {
   const props = mapStateToProps(initialState);
   expect(props.state).toHaveProperty('maxBlock');
   expect(typeof props.state.maxBlock).toBe('number');
 });
 
-it('mapStateToProps should return un Object that contains the rollback key', () => {
+it('mapStateToProps should return an Object that contains the rollback key', () => {
   const props = mapStateToProps(initialState);
   expect(props.state).toHaveProperty('rollback');
   expect(typeof props.state.rollback).toBe('number');
 });
 
-it('mapStateToProps should return un Object that contains the status key', () => {
+it('mapStateToProps should return an Object that contains the status key', () => {
   const props = mapStateToProps(initialState);
   expect(props.state).toHaveProperty('status');
   expect(typeof props.state.status).toBe('string');
 });
 
-it('mapStateToProps should return un Object that contains the wall key', () => {
+it('mapStateToProps should return an Object that contains the wall key', () => {
   const props = mapStateToProps(initialState);
   expect(props.state).toHaveProperty('wall');
   expect(Array.isArray(props.state.wall)).toBe(true);
--- src/components/App/App.test.js
+++ src/components/App/App.test.js
@@ -2,9 +2,32 @@ import React from 'react';
 import ShallowRenderer from 'react-test-renderer/shallow';
 import { App } from './index';
 
-it('renders without crashing', () => {
+it('renders Welcome without crashing', () => {
   const dispatch = jest.fn(action => action);
-  const state = { maxBlock: 0, rollback: 0, status: '', wall: [], };
+  const state = {
+    maxBlock: 0,
+    rollback: 0,
+    status: '',
+    wall: [],
+    welcome: true,
+  };
+  const renderer = new ShallowRenderer();
+  const tree = renderer.render(
+    <App state={state} dispatch={dispatch} />
+  );
+
+  expect(tree).toMatchSnapshot();
+});
+
+it('renders Wall without crashing', () => {
+  const dispatch = jest.fn(action => action);
+  const state = {
+    maxBlock: 0,
+    rollback: 0,
+    status: '',
+    wall: [],
+    welcome: false,
+  };
   const renderer = new ShallowRenderer();
   const tree = renderer.render(
     <App state={state} dispatch={dispatch} />
