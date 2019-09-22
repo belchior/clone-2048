68819c73b09f6c587b3e986a08bec149f328dfda
Deployment Bot (from Travis CI) <deploy@travis-ci.org>
2019-09-22 20:56:15 +0000

    Deploy belchior/clone-2048 to github.com/belchior/clone-2048.git:gh-pages

---
+++ precache-manifest.69f706a8af3d66f8a37c8827f09ac3b9.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ffe87a90c4041c2848d7a28c8c55a48c",
+    "url": "/clone-2048/index.html"
+  },
+  {
+    "revision": "f7db3f339cf8d5bf5d07",
+    "url": "/clone-2048/static/css/2.0971e70a.chunk.css"
+  },
+  {
+    "revision": "3bff7338a42cde29fc72",
+    "url": "/clone-2048/static/css/main.38ccd11f.chunk.css"
+  },
+  {
+    "revision": "f7db3f339cf8d5bf5d07",
+    "url": "/clone-2048/static/js/2.4cb499f0.chunk.js"
+  },
+  {
+    "revision": "3bff7338a42cde29fc72",
+    "url": "/clone-2048/static/js/main.dfd4f487.chunk.js"
+  },
+  {
+    "revision": "52b379ff1c9a2aaf03e3",
+    "url": "/clone-2048/static/js/runtime~main.16608bad.js"
+  },
+  {
+    "revision": "b8d1a0910468085af6c807892245f525",
+    "url": "/clone-2048/static/media/MontserratAlternates-Regular.b8d1a091.ttf"
+  },
+  {
+    "revision": "67712dffe9a6ee686ad5a2268c79474d",
+    "url": "/clone-2048/static/media/clone-2048.67712dff.svg"
+  },
+  {
+    "revision": "79531950e6aa22fee73275b9dc03cd0d",
+    "url": "/clone-2048/static/media/icon-github.79531950.svg"
+  },
+  {
+    "revision": "cd8e0b5c368cd7d983422d4d1713f770",
+    "url": "/clone-2048/static/media/icon-hamburger-menu.cd8e0b5c.svg"
+  }
+]);
